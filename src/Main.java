e46c52b492215f5e040d77515e0dc6e8ffb363b0
Leonardo Almeida <leonardoaesilva@outlook.com>
2022-12-19 18:53:06 -0300

    realizando uma jogada e validando-a

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,9 @@
+import java.util.Scanner;
+
 class JogoDaVelha {
+    boolean turnoJ1 = true;
+    char simbolo = '-';
+
     public void inicializarTabuleiro(char[][] tabuleiro) {
         for (int linha = 0; linha < tabuleiro.length; linha++) {
             for (int coluna = 0; coluna < tabuleiro[linha].length; coluna++) {
@@ -15,6 +20,49 @@ class JogoDaVelha {
             System.out.println();
         }
     }
+
+    private boolean validarJogada(char[][] tabuleiro, int l, int c) {
+        if (l < 0 || l > tabuleiro.length || c < 0 || c > tabuleiro.length) {
+            System.out.println("Posição inválida!");
+        }
+
+        for (int linha = 0; linha < tabuleiro.length; linha++) {
+            for (int coluna = 0; coluna < tabuleiro[linha].length; coluna++) {
+                if (tabuleiro[linha][coluna] != '#') {
+                        System.out.println("Uma jogada nessa posição já foi feita!");
+                } else {
+                    break;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public void realizarJogada(char[][] tabuleiro) {
+        if (turnoJ1) {
+            simbolo = 'X';
+        } else {
+            simbolo = 'O';
+        }
+
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.print("Jogador(a) " + simbolo + ", insira a posição na linha que deseja jogar (0, 1 ou 2): ");
+            int jogadaLinha = scanner.nextInt();
+            System.out.print("Jogador(a) " + simbolo + ", insira a posição na coluna que deseja jogar (0, 1 ou 2): ");
+            int jogadaColuna = scanner.nextInt();
+
+            boolean jogadaValida = validarJogada(tabuleiro, jogadaLinha, jogadaColuna);
+
+            if(jogadaValida) {
+                tabuleiro[jogadaLinha][jogadaColuna] = simbolo;
+            }
+
+            desenharTabuleiro(tabuleiro);
+        }
+    }
 }
 
 public class Main {
@@ -23,6 +71,6 @@ public class Main {
         JogoDaVelha jogo = new JogoDaVelha();
 
         jogo.inicializarTabuleiro(tabuleiro);
-        jogo.desenharTabuleiro(tabuleiro);
+        jogo.realizarJogada(tabuleiro);
     }
 }
