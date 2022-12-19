b2dcb389fe185935a5ff36f59cfde88fac1d06e3
Leonardo Almeida <leonardoaesilva@outlook.com>
2022-12-19 17:56:44 -0300

    iniciando e desenhando o tabuleiro

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+class JogoDaVelha {
+    public void inicializarTabuleiro(char[][] tabuleiro) {
+        for (int linha = 0; linha < tabuleiro.length; linha++) {
+            for (int coluna = 0; coluna < tabuleiro[linha].length; coluna++) {
+                tabuleiro[linha][coluna] = '#';
+            }
+        }
+    }
+
+    public void desenharTabuleiro(char[][] tabuleiro) {
+        for (char[] linha : tabuleiro) {
+            for (char coluna : linha) {
+                System.out.print(coluna + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        char[][] tabuleiro = new char[3][3];
+        JogoDaVelha jogo = new JogoDaVelha();
+
+        jogo.inicializarTabuleiro(tabuleiro);
+        jogo.desenharTabuleiro(tabuleiro);
     }
 }
